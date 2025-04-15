0208c1551fb5976c26a10fbe090da1984fff324a
matchstck <ceadpool15@gmail.com>
2025-04-15 12:12:46 +1000

    wqe2qw

---
+++ MMProject/Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---------- Audio Clip ----------")]
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip checkpoint;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
+
+
