633ebb8ce5567a28d8580e5a65668960736e8bb9
JustSoManyCupz <justsomanycupz@gmail.com>
2025-04-17 13:55:52 +1000

    Hopefuly(fixs

--- MMProject/Assets/script/Transistion.cs
+++ MMProject/Assets/script/Transistion.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Transistion : MonoBehaviour
 {
+
+    void Start()
+    {
+       
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
-            SceneManager.LoadScene(3);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            SceneManager.LoadScene(0);
+            Cursor.visible = true;
+
+            // Optionally, allow the cursor to move freely
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
+
+    
 }
