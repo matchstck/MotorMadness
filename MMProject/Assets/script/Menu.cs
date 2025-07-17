d93c9cc720aabaedf33b583c11edad4f0d0756da
matchstck <ceadpool15@gmail.com>
2025-07-17 15:40:53 +1000

    yip

--- MMProject/Assets/script/Menu.cs
+++ MMProject/Assets/script/Menu.cs
@@ -8,9 +8,13 @@ public class Menu : MonoBehaviour
     public void OnPlayButton()
     {
         SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+
     }
     public void OnQuitButton()
     {
         Application.Quit();
     }
 }
+
